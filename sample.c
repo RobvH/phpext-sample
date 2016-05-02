8993c439f87362f3ec0e7ced99512d813a2c9e04
Rob van Hoose <robertbvanhoose@gmail.com>
2016-05-02 01:55:38 -0400

    Added sample_array_range to demonstrate use of return_value_used

--- sample.c
+++ sample.c
@@ -17,11 +17,34 @@ PHP_FUNCTION(sample_return_long)
 	RETURN_LONG(42);	/* reference table on pg 69 */
 }
 
+PHP_FUNCTION(sample_array_range)
+{
+	if (return_value_used) {
+		int i;
+		/* Return an array from 0 - 999 */
+		array_init(return_value);
+
+		for(i = 0; i < 1000; ++i) {
+			add_next_index_long(return_value, i);
+		}
+
+		return;
+	}
+
+	/* Save the effort */
+	php_printf("Static return-only function called without processing output.\n");
+	php_error_docref(NULL TSRMLS_CC, E_NOTICE, 
+		"Static return-only function called without processing output.");
+
+	RETURN_NULL();
+}
+
 static zend_function_entry php_sample_functions[] = {   /* function_entry seems to be replaced with zend_function_entry */
 	PHP_FE(sample_hello_world,         NULL)
 	PHP_FALIAS(sample_hi, sample_hello_world, NULL)
 	PHP_FE(sample_long, NULL)
 	PHP_FE(sample_return_long, NULL)
+	PHP_FE(sample_array_range, NULL)
 	{ NULL, NULL, NULL }
 };
 
