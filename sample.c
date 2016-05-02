f94e002691d13faf5d59432b3d2ca3f0e902e753
Rob van Hoose <robertbvanhoose@gmail.com>
2016-05-02 01:42:53 -0400

    Added non-returning sample_long() function to demostrate RETVAL_LONG

--- sample.c
+++ sample.c
@@ -5,9 +5,17 @@ PHP_FUNCTION(sample_hello_world)
 	php_printf("Hello World!\n");
 }
 
+PHP_FUNCTION(sample_long)
+{
+	RETVAL_LONG(42);	/* does not return */
+	php_printf("The answer.\n");
+	return;
+}
+
 static zend_function_entry php_sample_functions[] = {   /* function_entry seems to be replaced with zend_function_entry */
 	PHP_FE(sample_hello_world,         NULL)
 	PHP_FALIAS(sample_hi, sample_hello_world, NULL)
+	PHP_FE(sample_long, NULL)
 	{ NULL, NULL, NULL }
 };
 
